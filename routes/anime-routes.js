90b557b25fa495bc8fde722e6653b4963a66a573
Ali Buali <alibuali3660@gmail.com>
2025-08-04 15:02:01 +0300

    lowercase user route

--- routes/anime-routes.js
+++ routes/anime-routes.js
@@ -1,5 +1,5 @@
 const Anime = require ("../models/anime")
-const User = require ("../models/User")
+const User = require ("../models/user")
 const router = require("express").Router()
 
 router.get("/user",async(req,res)=>{
